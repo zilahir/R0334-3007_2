f146e5b5205219567db1b941f63250f3c1892be4
Richard Zilahi <richard.zilahi@varjo.com>
2022-08-25 14:16:38 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'R0334-3007',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
